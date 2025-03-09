2cdd662098930790dd95e57b09373072e3fd40a3
raunak-garhwal <raunakgarhwal2003@gmail.com>
2025-03-10 03:29:16 +0530

    new file added

---
+++ General Questions/Prime Number.py
@@ -0,0 +1,28 @@
+import math
+
+def is_prime(n):
+    """Check if a number is prime."""
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def print_primes_up_to_n(n):
+    """Print all prime numbers up to N."""
+    primes = []
+    for i in range(2, n+1):
+        if is_prime(i):
+            primes.append(i)
+    print("Prime numbers up to", n, ":", primes)
+
+# Example Usage
+num = int(input("Enter a number to check if it's prime: "))
+if is_prime(num):
+    print(num, "is a prime number.")
+else:
+    print(num, "is not a prime number.")
+
+n = int(input("Enter a number to print all primes up to N: "))
+print_primes_up_to_n(n)
